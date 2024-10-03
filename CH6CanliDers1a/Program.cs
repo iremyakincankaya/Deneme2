92443b8a369b4a7b134a2c458be22bbfff259be4
iremyakincankaya <yakincanirem@gmail.com>
2024-10-03 19:24:56 +0300

    Add project files.

---
+++ CH6CanliDers1a/Program.cs
@@ -0,0 +1,11 @@
+﻿//cw [TAB]
+Console.WriteLine("Hello World");
+
+
+// programı calistirma F5(debug) ya da VTRL+F5(debugsız) yani gercek hayat senaryoyu gösterir
+
+// bir büyük projede calısıyorsun ve bi yer eksk kaldı unutmak istemiyorsun
+
+int toplam = 2 + 2;
+
+if 
